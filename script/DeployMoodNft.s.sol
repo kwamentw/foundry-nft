8447404933ba5b23e486431c2617fdd2602ff2da
KWAME <antwi1496@hotmail.com>
2023-12-19 03:58:30 -0600

    first commit

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18 ;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DeployMoodNft is Script{
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+        console.log(sadSvg);
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
+
+    function svgToImageUri(string memory svg) public pure returns (string memory){
+        string memory baseURL = "data: Image/svg+xml;base64";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
